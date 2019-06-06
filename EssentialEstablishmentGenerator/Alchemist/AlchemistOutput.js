f13576ebe38c6ca90a11a9e657ad2b72c5596475
Victor Glindås <glinkis@gmail.com>
2019-06-06 10:48:55 +0200

    Fix broken import

--- EssentialEstablishmentGenerator/Alchemist/AlchemistOutput.js
+++ EssentialEstablishmentGenerator/Alchemist/AlchemistOutput.js
@@ -2,7 +2,7 @@ import { link, replace, linkReplace, pragma } from '../../src/engine/html'
 import { randomValue } from '../../src/engine/rolls'
 import { set, get, unset } from '../../src/engine/story'
 
-import { alchemist } from './js/alchemistData'
+import { alchemistData } from './js/alchemistData'
 import { ChemistTalk } from './ChemistTalk'
 
 const TownMicroEventsOutput = () => ''
@@ -32,7 +32,7 @@ export function AlchemistOutput () {
 
   return pragma`<h1>${$alchemist.name}</h1>${TownMicroEventsOutput()}<span class="firstcharacter">Y</span>ou enter ${$alchemist.name} and see the ${$chemist.descriptor} chemist is ${$chemist.idle.seededrandom()}. The ${$alchemist.size} room is ${$alchemist.cleanliness}. The ${$chemist.weight} chemist ${randomValue($chemist.greeting)}, and ${randomValue(['saunters', 'walks', 'strolls', 'walks', 'slowly walks', `drags ${$chemist.hisher} feet`, 'swaggers', 'shuffles', 'quickly walks', 'struts', 'comes'])} over to you and introduces ${$chemist.himherself} as <<profile $chemist>>, the ${$chemist.owner} of the shop, and asks what ${$chemist.heshe} can do for you.
 
-${setup.closestMatch(alchemist.get.lookAround($alchemist), 'note', 'cleanliness', 'wealth', $alchemist.roll.cleanliness, $alchemist.roll.wealth)}
+${setup.closestMatch(alchemistData.get.lookAround($alchemist), 'note', 'cleanliness', 'wealth', $alchemist.roll.cleanliness, $alchemist.roll.wealth)}
 ${linkReplace(`<h4>Talk with ${$chemist.name}</h4>`, () => `<h3>${$chemist.name}</h3>${ChemistTalk()}`)}
 ${link('<h4>Generate plothook</h4>', generatePlothook)}<span id="chemistMission"></span>
 ${RandomPotion()}
