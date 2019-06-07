f535a3668efd497b2a768214fef7c309bed97221
Victor Glindås <glinkis@gmail.com>
2019-06-07 23:13:51 +0200

    Refactor createAlchemistName

--- EssentialEstablishmentGenerator/Alchemist/js/createAlchemistName.js
+++ EssentialEstablishmentGenerator/Alchemist/js/createAlchemistName.js
@@ -1,29 +1,27 @@
+import { randomValue } from "../../../src/engine/rolls"
+
+/**
+ * @param {string} chemistFirstName
+ * @returns {string}
+ */
 export function createAlchemistName (chemistFirstName) {
-  const alchemistNameRoll = dice(1, 5)
-  let name
-  const adjective = ['Bubbling', 'Spicy', 'Soggy', 'Fizzy', 'Liquid', 'Fluorescent', 'Clear', 'Alcoholic', 'Abyssal', 'Angelic', 'Elven', 'Measured', 'Marked', 'Glass', 'Glass', 'Copper', 'Corked', 'Burning', 'Red', 'Blue', 'Green', 'Gold', 'Yellow', 'Vile', 'Genuine', 'Original'].seededrandom()
-  const noun = ['Potion', 'Liquid', 'Fumes', 'Bottle', 'Vial', 'Firewater', 'Mortar and Pestle', 'Lab', 'Laboratory', 'Chemist', 'Alchemist', 'Brewer', 'Lotion', 'Wishes'].seededrandom()
-  const adjective2 = ['Bubbling', 'Spicy', 'Soggy', 'Fizzy', 'Liquid', 'Fluorescent', 'Clear', 'Alcoholic', 'Abyssal', 'Angelic', 'Elven', 'Measured', 'Marked', 'Glass', 'Glass', 'Copper', 'Corked', 'Burning', 'Red', 'Blue', 'Green', 'Gold', 'Yellow', 'Vile', 'Genuine', 'Original'].seededrandom()
-  const rider = ['Chemist', 'Alchemist', 'Potion Shop', 'Potionery', 'Ointmentary', 'Juice Bar', 'Lab', 'Laboratory', 'Secret Lair'].seededrandom()
-  switch (alchemistNameRoll) {
+  const adjective = randomValue([`Bubbling`, `Spicy`, `Soggy`, `Fizzy`, `Liquid`, `Fluorescent`, `Clear`, `Alcoholic`, `Abyssal`, `Angelic`, `Elven`, `Measured`, `Marked`, `Glass`, `Glass`, `Copper`, `Corked`, `Burning`, `Red`, `Blue`, `Green`, `Gold`, `Yellow`, `Vile`, `Genuine`, `Original`])
+  const noun = randomValue([`Potion`, `Liquid`, `Fumes`, `Bottle`, `Vial`, `Firewater`, `Mortar and Pestle`, `Lab`, `Laboratory`, `Chemist`, `Alchemist`, `Brewer`, `Lotion`, `Wishes`])
+  const adjective2 = randomValue([`Bubbling`, `Spicy`, `Soggy`, `Fizzy`, `Liquid`, `Fluorescent`, `Clear`, `Alcoholic`, `Abyssal`, `Angelic`, `Elven`, `Measured`, `Marked`, `Glass`, `Glass`, `Copper`, `Corked`, `Burning`, `Red`, `Blue`, `Green`, `Gold`, `Yellow`, `Vile`, `Genuine`, `Original`])
+  const rider = randomValue([`Chemist`, `Alchemist`, `Potion Shop`, `Potionery`, `Ointmentary`, `Juice Bar`, `Lab`, `Laboratory`, `Secret Lair`])
+
+  switch (dice(1, 5)) {
     case 1:
-      name = 'The ' + adjective + ' ' + noun
-      break
+      return `The ${adjective} ${noun}`
     case 2:
-      name = chemistFirstName + ' and ' + adjective2
-      // name = 'The ' + adjective + ' ' + noun
-      break
+      return `${chemistFirstName} and ${adjective2}`
     case 3:
-      name = 'The ' + noun + ' and ' + adjective2
-      break
+      return `The ${noun} and ${adjective2}`
     case 4:
-      name = 'The ' + adjective + ' ' + rider
-      break
+      return `The ${adjective} ${rider}`
     case 5:
-      name = adjective + ' ' + noun
-      break
+      return `${adjective} ${noun}`
     default:
-      name = 'The ' + adjective + ' Alchemist'
+      return `The ${adjective} Alchemist`
   }
-  return name
 }
