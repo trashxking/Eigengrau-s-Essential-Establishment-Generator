89d88758aeb782149e373ddaebf38fa51c4f2a7a
Victor Glindås <glinkis@gmail.com>
2019-06-08 15:57:51 +0200

    Replace

--- EssentialEstablishmentGenerator/Town/js/createNewBuilding.js
+++ EssentialEstablishmentGenerator/Town/js/createNewBuilding.js
@@ -1,8 +1,10 @@
+import { toUpperFirst } from "../../../src/engine/utils"
+
 export function createNewBuilding (town, buildingType, opts) {
   if (!town.buildings[buildingType]) {
     town.buildings[buildingType] = {}
   }
-  const building = setup[`create${buildingType.toUpperFirst()}`](town, opts)
+  const building = setup[`create${toUpperFirst(buildingType)}`](town, opts)
   town.buildings[buildingType][building.key] = building
   return town
 }
