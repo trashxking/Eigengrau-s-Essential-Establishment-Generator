9fec0d50c6c948cec29f9f50f84034072f61b2fa
Victor Glindås <glinkis@gmail.com>
2019-06-08 18:04:30 +0200

    Focumentation fix

--- EssentialEstablishmentGenerator/Tools/contentsFetcher.js
+++ EssentialEstablishmentGenerator/Tools/contentsFetcher.js
@@ -1,6 +1,8 @@
+/**
+ * This is the function used to handle random encounters. It needs a lot of work, and will change.
+ */
 export function contentsFetcher (town, biome, keyTarget, contentsTarget, base) {
-  // this is the function used to handle random encounters. It needs a lot of work, and will change.
   const key = keyTarget.random()
-  const contents = contentsTarget[key](town, biome, base)
-  return contents
+  const contents = contentsTarget[key]
+  return contents(town, biome, base)
 }
