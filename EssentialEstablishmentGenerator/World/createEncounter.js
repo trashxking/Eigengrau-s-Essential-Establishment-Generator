1d34e9c79e048f19784e261fbc68a45b8ca3db2c
Victor Glindås <glinkis@gmail.com>
2019-06-08 17:25:30 +0200

    Modularize createEncounter

--- EssentialEstablishmentGenerator/World/createEncounter.js
+++ EssentialEstablishmentGenerator/World/createEncounter.js
@@ -1,10 +1,3 @@
-setup.createEncounter = function (town, biome) {
-  // console.log('scenario:')
-  // console.log(scenario)
-  const encounter = setup.misc[biome].create(town)
-  // weather: scenario.weather || createWeather(town, biome)
-  // scenario.weather = createWeather(town, biome, scenario.weather, 4)
-  // console.log(scenario)
-
-  return encounter
+export function createEncounter (town, biome) {
+  return setup.misc[biome].create(town)
 }
