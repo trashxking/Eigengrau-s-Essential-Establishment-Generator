35dd405e5166a062323d53b6d852d618e1950997
Victor Glindås <glinkis@gmail.com>
2019-06-08 16:19:15 +0200

    Modularize guardData

--- EssentialEstablishmentGenerator/Town/js/createGuard.js
+++ EssentialEstablishmentGenerator/Town/js/createGuard.js
@@ -1,4 +1,5 @@
 import { createNPC } from "../../NPCGeneration/SetupNPC"
+import { guardData } from "./guardData"
 
 setup.createGuard = function (town) {
   console.groupCollapsed(`creating the guard...`)
@@ -11,27 +12,27 @@ setup.createGuard = function (town) {
       background: `soldier`
     }),
     passageName: `GuardOutput`,
-    livery: `${setup.guardData.primaryColours.seededrandom()} and ${setup.guardData.secondaryColours.seededrandom()} livery adorned with an image of ${setup.guardData.insignia.seededrandom()}`
+    livery: `${guardData.primaryColours.seededrandom()} and ${guardData.secondaryColours.seededrandom()} livery adorned with an image of ${guardData.insignia.seededrandom()}`
   }
 
   const nameRoll = random(1, 8)
   console.log(`nameRoll is ${nameRoll}`)
   if (nameRoll === 1) {
-    guard.name = `The ${setup.guardData.name.group.seededrandom()} of ${town.name}`
+    guard.name = `The ${guardData.name.group.seededrandom()} of ${town.name}`
   } else if (nameRoll === 2) {
-    guard.name = `The ${town.name} ${setup.guardData.name.group.seededrandom()}`
+    guard.name = `The ${town.name} ${guardData.name.group.seededrandom()}`
   } else if (nameRoll === 3) {
-    guard.name = `The ${setup.guardData.name.group.seededrandom()} of ${setup.guardData.name.alternateAdjective.seededrandom()}`
+    guard.name = `The ${guardData.name.group.seededrandom()} of ${guardData.name.alternateAdjective.seededrandom()}`
   } else if (nameRoll === 4) {
-    guard.name = `The ${setup.guardData.name.adjective.seededrandom()} ${setup.guardData.name.group.seededrandom()}`
+    guard.name = `The ${guardData.name.adjective.seededrandom()} ${guardData.name.group.seededrandom()}`
   } else if (nameRoll === 5) {
-    guard.name = `The ${setup.guardData.name.adjective.seededrandom()} ${setup.guardData.name.noun.seededrandom()}`
+    guard.name = `The ${guardData.name.adjective.seededrandom()} ${guardData.name.noun.seededrandom()}`
   } else if (nameRoll === 6) {
-    guard.name = `The ${setup.guardData.name.adjective.seededrandom()} ${setup.guardData.name.noun.seededrandom()} of ${setup.guardData.name.alternateAdjective.seededrandom()}`
+    guard.name = `The ${guardData.name.adjective.seededrandom()} ${guardData.name.noun.seededrandom()} of ${guardData.name.alternateAdjective.seededrandom()}`
   } else if (nameRoll === 7) {
-    guard.name = `The ${setup.guardData.name.adjective.seededrandom()} ${setup.guardData.name.noun.seededrandom()} of ${town.name}`
+    guard.name = `The ${guardData.name.adjective.seededrandom()} ${guardData.name.noun.seededrandom()} of ${town.name}`
   } else if (nameRoll === 8) {
-    guard.name = `The ${setup.guardData.name.group.seededrandom()} ${setup.guardData.name.noun.seededrandom()} of ${town.name}`
+    guard.name = `The ${guardData.name.group.seededrandom()} ${guardData.name.noun.seededrandom()} of ${town.name}`
   }
   console.log(`${guard.name} is the name of the guard.`)
   console.groupEnd()
