c83a6eec1450aa4e7a90daca6351cc825328a8d7
Victor Glindås <glinkis@gmail.com>
2019-06-08 17:19:17 +0200

    Modularize weather data

--- EssentialEstablishmentGenerator/Town/Weather/js/renderWeather.js
+++ EssentialEstablishmentGenerator/Town/Weather/js/renderWeather.js
@@ -1,6 +1,7 @@
 import { townData } from "../../js/townData"
 import { createWeather } from "./createWeather"
 import { toCelsius } from "./toCelsius"
+import { weatherData } from "./weather"
 
 export function renderWeather (town, biome, weather) {
   console.log(`Rendering weather...`)
@@ -57,17 +58,17 @@ export function renderWeather (town, biome, weather) {
   if (weather.timer.precipitation < 1) {
     console.log(`Resetting precipitation timer...`)
     weather.roll.precipitation = random(1, 100)
-    weather.precipitation = setup.weather.precipitationLevel[weather.precipitationLevel](weather)
+    weather.precipitation = weatherData.precipitationLevel[weather.precipitationLevel](weather)
   }
 
   if (weather.precipitation === true && weather.temperature <= 32) {
     console.log(`Rolling on the freezing table...`)
     weather.roll.precipitationIntensity = random(1, 100)
-    setup.weather.precipitationIntensity[weather.precipitationIntensity].freezing(weather)
+    weatherData.precipitationIntensity[weather.precipitationIntensity].freezing(weather)
   } else if (weather.precipitation === true) {
     console.log(`Rolling on the raining table...`)
     weather.roll.precipitationIntensity = random(1, 100)
-    setup.weather.precipitationIntensity[weather.precipitationIntensity].raining(weather)
+    weatherData.precipitationIntensity[weather.precipitationIntensity].raining(weather)
   } else {
     console.log(`Clear day!`)
     weather.precipitation = `no precipitation`
@@ -77,10 +78,10 @@ export function renderWeather (town, biome, weather) {
   if (weather.timer.cloud < 1) {
     console.log(`Resetting cloud timer...`)
     weather.roll.cloud = random(1, 100)
-    setup.weather.precipitationIntensity[weather.precipitationIntensity].cloud(weather)
+    weatherData.precipitationIntensity[weather.precipitationIntensity].cloud(weather)
   }
 
-  weather.readout.precipitation = setup.weather.precipitationDescriptors[weather.precipitation].seededrandom()
+  weather.readout.precipitation = weatherData.precipitationDescriptors[weather.precipitation].seededrandom()
   if (weather.precipitation !== `no precipitation` && weather.timer.precipitation > 18) {
     weather.readout.precipitation += `. It doesn't look like it'll be clearing up today`
   } else if (weather.precipitation !== `no precipitation` && weather.timer.precipitation > 12) {
@@ -88,10 +89,10 @@ export function renderWeather (town, biome, weather) {
   } else if (weather.precipitation !== `no precipitation` && weather.timer.precipitation <= 2) {
     weather.readout.precipitation += `. It's clearing up pretty quickly, though`
   }
-  weather.readout.cloud = setup.weather.cloudIntensityDescriptors[weather.cloudIntensity].seededrandom()
+  weather.readout.cloud = weatherData.cloudIntensityDescriptors[weather.cloudIntensity].seededrandom()
 
   console.log(`Rendering temperature...`)
-  for (const array of setup.weather.temperatureDescriptors) {
+  for (const array of weatherData.temperatureDescriptors) {
     // console.log(array)
     if (weather.temperature >= array[0]) {
       weather.readout.temperature = `<span class=tip title=${JSON.stringify(`${toCelsius(weather.temperature)}, to be precise.`)}><b>${array[1]}</b></span><<run setup.tippy("span")>>`
