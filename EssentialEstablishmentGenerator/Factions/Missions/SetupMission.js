5a94cdc4cb2e7c3c7eaaa3b2de12fa71f2ae059a
Victor Glindås <glinkis@gmail.com>
2019-06-08 19:52:59 +0200

    Modularize merchantMission

--- EssentialEstablishmentGenerator/Factions/Missions/SetupMission.js
+++ EssentialEstablishmentGenerator/Factions/Missions/SetupMission.js
@@ -1,13 +1,15 @@
+import { merchantMission } from "./merchantMission"
+
 setup.createMission = function (town, base) {
   // var category = ['thieves', 'wizards', 'bartender', 'alchemist', 'merchant']
-  const difficulty = ['easy', 'easy', 'easy', 'medium', 'medium', 'hard']
+  const difficulty = [`easy`, `easy`, `easy`, `medium`, `medium`, `hard`]
   // var difficultyText
   let details
   let readout
   let targetNPC
   let targetLocation
   const mission = Object.assign({
-    category: ['thieves', 'alchemist'].seededrandom(),
+    category: [`thieves`, `alchemist`].seededrandom(),
     difficulty: difficulty.seededrandom(),
     readout,
     details,
@@ -16,17 +18,17 @@ setup.createMission = function (town, base) {
   }, base)
 
   switch (mission.category) {
-    case 'thieves':
+    case `thieves`:
       setup.thievesMission(mission)
       break
-    case 'alchemist':
+    case `alchemist`:
       setup.alchemistMission(mission)
       break
-    case 'bartender':
+    case `bartender`:
       setup.bartenderMission(mission)
       break
-    case 'merchant':
-      setup.merchantMission(mission)
+    case `merchant`:
+      merchantMission(mission)
       break
   }
 
