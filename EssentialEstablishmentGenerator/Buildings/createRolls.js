e4051ec004837262c73016eedbebab82690bf424
Victor Glindås <glinkis@gmail.com>
2019-06-08 09:09:14 +0200

    Clean up createRolls

--- EssentialEstablishmentGenerator/Buildings/createRolls.js
+++ EssentialEstablishmentGenerator/Buildings/createRolls.js
@@ -1,48 +1,36 @@
-setup.createRolls = function (object, propNamesArray) {
-  console.groupCollapsed('Creating rolls...')
-  console.log({ object })
-  // propNamesArray is expected to be an array.
-  // propNamesArray = propNamesArray || ['wealth', 'reputation', 'sin', 'diversity', 'magic', 'size', 'population', 'roughness', 'cleanliness', 'expertise', 'activity']
+import { randomRange } from "../../src/engine/rolls"
 
-  // for (var i = 0; i < propNamesArray.length; i++) {
-  if (typeof propNamesArray === 'string' && !object[propNamesArray]) {
-    object[propNamesArray] = random(1, 100)
-  } else if (Array.isArray(propNamesArray)) {
-    propNamesArray.forEach(function (propName) {
-    // var propName = propNamesArray[i]
+/**
+ * @param {{ [x: string]: number; }} object
+ * @param {string | string[] | Record<string, any>} propNames
+ */
+setup.createRolls = function (object, propNames) {
+  console.groupCollapsed(`Creating rolls...`)
+  console.log({ object })
+
+  if (typeof propNames === `string` && !object[propNames]) {
+    object[propNames] = randomRange(1, 100)
+  } else if (Array.isArray(propNames)) {
+    for (const propName of propNames) {
       if (!Object.keys(object).includes(propName)) {
-        console.log('No ' + propName + ' roll value! Creating one...')
+        console.log(`No ${propName} roll value! Creating one...`)
         console.log({ object })
-        object[propName] = random(1, 100)
+        object[propName] = randomRange(1, 100)
       }
-      // }
-    })
-  } else if (typeof propNamesArray === 'object') {
-    Object.keys(propNamesArray).forEach(function (propName) {
+    }
+  } else if (typeof propNames === `object`) {
+    for (const propName of Object.keys(propNames)) {
       if (!Object.keys(object).includes(propName)) {
-        console.log('No ' + propName + ' roll value! Creating one...')
+        console.log(`No ${propName} roll value! Creating one...`)
         console.log({ object })
-        object[propName] = random(1, 100)
+        object[propName] = randomRange(1, 100)
       }
-    })
+    }
   } else {
-    console.error('Expected a string, object, or array!')
+    console.error(`Expected a string, object, or array!`)
     console.log({ object })
-    console.log({ propNamesArray })
+    console.log({ propNames })
   }
-  // object[rollLocation] = {
-  //   magic: (Math.floor(randomFloat(1) * 80) + 20),
-  //   size: (Math.floor(randomFloat(1) * 80) + 20),
-  //   diversity: (Math.floor(randomFloat(1) * 80) + 20),
-  //   wealth: random(1, 100),
-  //   population: random(1, 100),
-  //   reputation: random(1, 100),
-  //   sin: random(1, 100),
-  //   roughness: random(1, 100),
-  //   cleanliness: random(1, 100),
-  //   expertise: random(1, 100),
-  //   activity: random(1, 100)
-  // }
-  // return object
+
   console.groupEnd()
 }
