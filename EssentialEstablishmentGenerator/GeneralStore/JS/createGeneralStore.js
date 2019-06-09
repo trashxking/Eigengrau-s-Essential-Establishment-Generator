1ba9e14aa0d363503bfe12fd9564ace0af0b61ea
Victor Glindås <glinkis@gmail.com>
2019-06-09 10:55:13 +0200

    Revert GeneralStore

--- EssentialEstablishmentGenerator/GeneralStore/JS/createGeneralStore.js
+++ EssentialEstablishmentGenerator/GeneralStore/JS/createGeneralStore.js
@@ -1,39 +1,34 @@
-import { createBuilding } from "../../Buildings/createBuilding"
-import { defineRollDataGetter } from "../../Tools/defineRollDataGetter"
-import { createNPC } from "../../NPCGeneration/SetupNPC"
-import { createGeneralStoreName } from "./createGeneralStoreName"
-import { GeneralStoreData } from "./GeneralStoreData"
-import { GeneralStoreModifiers } from "./GeneralStoreModifiers"
 
-export function createGeneralStore (town, opts = {}) {
-  const GeneralStore = (opts.newBuilding || createBuilding)(town, `GeneralStore`)
-  console.groupCollapsed(`General Store loading...`)
-  GeneralStore.shopkeep = (opts[`newShopkeep`] || createNPC)(town, {
-    profession: `merchant`,
-    mundane: [`pliers`, `tins`, `twine`, `cups`, `spoons`, `pans`, `chairs`, `cushions`],
-    greeting: [`nods at you`, `welcomes you warmly`, `smiles and greets you`, `raises a hand with a wave`, `checks you out for just a moment before smiling at you`],
-    owner: [`owner`, `caretaker`, `proud owner`, `proprietor`, `current owner`, `manager`, `assistant manager`, `acting manager`].seededrandom()
+setup.createGeneralStore = function (town, opts) {
+  opts = opts || {}
+  const GeneralStore = (opts['newBuilding'] || setup.createBuilding)(town, 'GeneralStore')
+  console.groupCollapsed('General Store loading...')
+  GeneralStore.shopkeep = (opts['newShopkeep'] || setup.createNPC)(town, {
+    profession: 'merchant',
+    mundane: ['pliers', 'tins', 'twine', 'cups', 'spoons', 'pans', 'chairs', 'cushions'],
+    greeting: ['nods at you', 'welcomes you warmly', 'smiles and greets you', 'raises a hand with a wave', 'checks you out for just a moment before smiling at you'],
+    owner: ['owner', 'caretaker', 'proud owner', 'proprietor', 'current owner', 'manager', 'assistant manager', 'acting manager'].seededrandom()
   })
   Object.assign(GeneralStore, {
-    note: GeneralStoreData.get.note(GeneralStore),
-    wordNoun: [`general store`, `shop`].seededrandom(),
-    crud: GeneralStoreData.crud,
-    notableFeature: `wide range of goods on sale`,
-    passageName: `GeneralStoreOutput`,
-    initPassage: `InitGeneralStore`,
-    buildingType: `GeneralStore`
+    note: setup.GeneralStore.get.note(GeneralStore),
+    wordNoun: ['general store', 'shop'].seededrandom(),
+    crud: setup.GeneralStore.crud,
+    notableFeature: 'wide range of goods on sale',
+    passageName: 'GeneralStoreOutput',
+    initPassage: 'InitGeneralStore',
+    buildingType: 'GeneralStore'
   })
 
-  createGeneralStoreName(town, GeneralStore)
-  GeneralStore.wealth = ``
-  GeneralStore.size = ``
-  GeneralStore.cleanliness = ``
-  GeneralStore.expertise = ``
-  GeneralStoreModifiers(town, GeneralStore)
+  setup.createGeneralStoreName(town, GeneralStore)
+  GeneralStore.wealth = ''
+  GeneralStore.size = ''
+  GeneralStore.cleanliness = ''
+  GeneralStore.expertise = ''
+  setup.GeneralStoreModifiers(town, GeneralStore)
 
-  const rollDataVariables = [`wealth`, `size`, `cleanliness`, `expertise`]
+  const rollDataVariables = ['wealth', 'size', 'cleanliness', 'expertise']
   rollDataVariables.forEach(function (propName) {
-    defineRollDataGetter(GeneralStore, GeneralStoreData.rollData, propName)
+    setup.defineRollDataGetter(GeneralStore, setup.GeneralStore.rollData, propName)
   })
   // setup.GeneralStoreRenders(GeneralStore)
   console.log(GeneralStore)
