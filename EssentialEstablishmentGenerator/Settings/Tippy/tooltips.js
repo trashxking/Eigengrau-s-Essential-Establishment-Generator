38de43f07cff6ab210703cb9f969a1375eb85676
Victor Glindås <glinkis@gmail.com>
2019-06-09 11:42:43 +0200

    Revert Settings

--- EssentialEstablishmentGenerator/Settings/Tippy/tooltips.js
+++ EssentialEstablishmentGenerator/Settings/Tippy/tooltips.js
@@ -1,12 +1,10 @@
-import { townData } from "../../Town/js/townData"
-
 /* global setup tippy jQuery settings */
 setup.profileTooltip = function (id, char) {
   jQuery(function () {
     const span = document.getElementById(id)
     if (span) {
-      span.title = (char.tippyDescription || `A ${char.weight} ${char.malefemale} ${char.raceAdjective} ${char.dndClass} with ${char.physicalTrait} called ${char.name}`)
-      tippy(`#${span.id}`)
+      span.title = (char.tippyDescription || 'A ' + char.weight + ' ' + char.malefemale + ' ' + char.raceAdjective + ' ' + char.dndClass + ' with ' + char.physicalTrait + ' called ' + char.name)
+      tippy('#' + span.id)
     }
   })
 }
@@ -16,7 +14,7 @@ setup.itemTooltip = function (id, item) {
     const span = document.getElementById(id)
     if (span) {
       span.title = item.description || item
-      tippy(`#${span.id}`)
+      tippy('#' + span.id)
     }
   })
 }
@@ -25,8 +23,8 @@ setup.profileAgeTooltip = function (id, char) {
   jQuery(function () {
     const span = document.getElementById(id)
     if (span) {
-      span.title = `${char.ageYears} years, to be exact.`
-      tippy(`#${span.id}`)
+      span.title = char.ageYears + ' years, to be exact.'
+      tippy('#' + span.id)
     }
   })
 }
@@ -39,17 +37,17 @@ setup.profileHeightTooltip = function (id, char, heightVar) {
     const span = document.getElementById(id)
     if (span) {
       if (settings.showMetric === true) {
-        span.title = `${(char.heightRoll * 0.0254).toFixed(2)}m`
-        tippy(`#${span.id}`)
+        span.title = (char.heightRoll * 0.0254).toFixed(2) + 'm'
+        tippy('#' + span.id)
       } else {
         const feet = Math.trunc(char.heightRoll / 12)
         const inches = Math.round(char.heightRoll - (Math.trunc(feet * 12)))
         if (inches === 0) {
-          span.title = `${feet}ft. `
+          span.title = feet + 'ft. '
         } else {
-          span.title = `${feet}ft. ${inches}"`
+          span.title = feet + 'ft. ' + inches + '"'
         }
-        tippy(`#${span.id}`)
+        tippy('#' + span.id)
       }
     }
   })
@@ -60,13 +58,13 @@ setup.profileWeightTooltip = function (id, char) {
     const span = document.getElementById(id)
     if (span) {
       if (settings.showMetric === true) {
-        span.title = `${(char.weightRoll / 2.2046).toFixed(1)}kg (with a BMI of ${char.bmi})`
-        tippy(`#${span.id}`)
+        span.title = (char.weightRoll / 2.2046).toFixed(1) + 'kg (with a BMI of ' + char.bmi + ')'
+        tippy('#' + span.id)
       } else {
-        span.title = `${char.weightRoll}lbs. (with a BMI of ${char.bmi})`
+        span.title = char.weightRoll + 'lbs. (with a BMI of ' + char.bmi + ')'
       }
 
-      tippy(`#${span.id}`)
+      tippy('#' + span.id)
     }
   }
   )
@@ -76,8 +74,8 @@ setup.buildingTooltip = function (id, building) {
   jQuery(function () {
     const span = document.getElementById(id)
     if (span) {
-      span.title = building.tippyDescription || `A ${building.size || building._size} ${building.wordNoun} that's ${building.cleanliness || building._cleanliness}, and is known for ${building.notableFeature}.`
-      tippy(`#${span.id}`)
+      span.title = building.tippyDescription || 'A ' + (building.size || building._size) + ' ' + building.wordNoun + " that's " + (building.cleanliness || building._cleanliness) + ', and is known for ' + building.notableFeature + '.'
+      tippy('#' + span.id)
     }
   })
 }
@@ -87,24 +85,24 @@ setup.politicsTooltip = function (id, type, town) {
     const span = document.getElementById(id)
     if (span) {
       switch (type) {
-        case `politicalIdeology`:
-          span.title = townData.politicalIdeology[town.politicalIdeology].data.description
+        case 'politicalIdeology':
+          span.title = setup.townData.politicalIdeology[town.politicalIdeology].data.description
           break
-        case `economicIdeology`:
-          span.title = townData.economicIdeology[town.economicIdeology].descriptors.tippy
+        case 'economicIdeology':
+          span.title = setup.townData.economicIdeology[town.economicIdeology].descriptors.tippy
           break
-        case `politicalSource`:
-          if (town.politicalSource === `absolute monarchy` || town.politicalSource === `constitutional monarchy`) {
-            if (town.politicalIdeology === `autocracy`) {
-              span.title = townData.politicalSource[town.politicalSource].autocracy.description
+        case 'politicalSource':
+          if (town.politicalSource === 'absolute monarchy' || town.politicalSource === 'constitutional monarchy') {
+            if (town.politicalIdeology === 'autocracy') {
+              span.title = setup.townData.politicalSource[town.politicalSource].autocracy.description
             } else {
-              span.title = townData.politicalSource[town.politicalSource][`default`].description
+              span.title = setup.townData.politicalSource[town.politicalSource]['default'].description
             }
           } else {
-            span.title = townData.politicalSource[town.politicalSource].description
+            span.title = setup.townData.politicalSource[town.politicalSource].description
           }
       }
-      tippy(`#${span.id}`)
+      tippy('#' + span.id)
     }
   })
 }
