f127d238442cd25d480932e35f271e8328689aa3
Victor Glindås <glinkis@gmail.com>
2019-06-09 10:54:04 +0200

    Revert docks

--- EssentialEstablishmentGenerator/Docks/createDocks.js
+++ EssentialEstablishmentGenerator/Docks/createDocks.js
@@ -1,45 +1,42 @@
-import { createBuilding } from "../Buildings/createBuilding"
-import { defineRollDataGetter } from "../Tools/defineRollDataGetter"
-import { docksData } from "./docks"
-
-export function createDocks (town, opts = {}) {
-  const docks = (opts.newBuilding || createBuilding)(town, `docks`)
+setup.createDocks = function (town, opts) {
+  opts = opts || {}
+  const docks = (opts['newBuilding'] || setup.createBuilding)(town, 'docks')
   Object.assign(docks, {
-    notableFeature: docksData.notableFeature.seededrandom(),
-    notice: docksData.notice.seededrandom(),
-    passageName: `DocksOutput`,
-    initPassage: `DocksOutput`,
-    buildingType: `docks`,
+    notableFeature: setup.docks.notableFeature.seededrandom(),
+    notice: setup.docks.notice.seededrandom(),
+    passageName: 'DocksOutput',
+    initPassage: 'DocksOutput',
+    buildingType: 'docks',
     needsWordNoun: false,
     dockName: setup.createNPC(town, {
       isShallow: true
     }),
-    wordNoun: [`docks`, `pier`, `wharf`, `dockyard`, `shipyard`, `quay`, `staithe`, `marina`, `jetty`, `harbor`, `berth`, `port`, `seaport`, `dockyard`].seededrandom(),
+    wordNoun: ['docks', 'pier', 'wharf', 'dockyard', 'shipyard', 'quay', 'staithe', 'marina', 'jetty', 'harbor', 'berth', 'port', 'seaport', 'dockyard'].seededrandom(),
     ships: {},
-    typePool: docksData.ships.typePool
+    typePool: setup.docks.ships.typePool
   })
 
-  docks.name = [`The${[` Old `, ` New `, ` `, ` `].seededrandom()}${[`${town.name} `, `${town.name} `, ` `, ` `, ` `].seededrandom()}${docks.wordNoun.toUpperFirst()}`, [`${docks.dockName.lastName} `, `${docks.dockName.firstName}'s `, `${[docks.dockName.firstName, docks.dockName.lastName].seededrandom()} Beach `].seededrandom() + docks.wordNoun.toUpperFirst()].seededrandom()
+  docks.name = ['The' + [' Old ', ' New ', ' ', ' '].seededrandom() + [town.name + ' ', town.name + ' ', ' ', ' ', ' '].seededrandom() + docks.wordNoun.toUpperFirst(), [docks.dockName.lastName + ' ', docks.dockName.firstName + "'s ", [docks.dockName.firstName, docks.dockName.lastName].seededrandom() + ' Beach '].seededrandom() + docks.wordNoun.toUpperFirst()].seededrandom()
 
   // docks.wealth = ''
-  docks.activity = ``
-  docks.size = ``
-  docks.cleanliness = ``
+  docks.activity = ''
+  docks.size = ''
+  docks.cleanliness = ''
 
-  const rollDataVariables = [`size`, `cleanliness`, `activity`]
+  const rollDataVariables = ['size', 'cleanliness', 'activity']
   rollDataVariables.forEach(function (propName) {
-    defineRollDataGetter(docks, docksData.rollData, propName)
+    setup.defineRollDataGetter(docks, setup.docks.rollData, propName)
   })
 
-  docks.sizeDescriptive = ``
-  docks.cleanlinessDescriptive = ``
-  docks.activityDescriptive = ``
+  docks.sizeDescriptive = ''
+  docks.cleanlinessDescriptive = ''
+  docks.activityDescriptive = ''
 
-  defineRollDataGetter(docks, docksData.rollData, `sizeDescriptive`, `size`, 2)
-  defineRollDataGetter(docks, docksData.rollData, `cleanlinessDescriptive`, `cleanliness`, 2)
-  defineRollDataGetter(docks, docksData.rollData, `activityDescriptive`, `activity`, 2)
+  setup.defineRollDataGetter(docks, setup.docks.rollData, 'sizeDescriptive', 'size', 2)
+  setup.defineRollDataGetter(docks, setup.docks.rollData, 'cleanlinessDescriptive', 'cleanliness', 2)
+  setup.defineRollDataGetter(docks, setup.docks.rollData, 'activityDescriptive', 'activity', 2)
 
-  docksData.ships.create(town, docks)
+  setup.docks.ships.create(town, docks)
 
   return docks
 }
